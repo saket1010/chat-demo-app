a55a9634e7f7d28a44d6ef38203b0dabc1dd6bd4
saket <saketbbu01@outlook.com>
2023-07-25 01:16:23 +0530

    new

--- src/main/java/com/example/chatdemoapp/ChatDemoAppApplication.java
+++ src/main/java/com/example/chatdemoapp/ChatDemoAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatDemoAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ChatDemoAppApplication.class, args);
     }
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,14 +1,41 @@
 var stompClient=null
-
-name=prompt("Enter name");
+var name=prompt("Enter name");
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 login(name);
+function scrollToEnd(block, duration) {
+    // if block not passed scroll to end of page
+    block = block || $("html, body");
+    duration = duration || 2000;
 
+    // you can pass also block's jQuery selector instead of jQuery object
+    if(typeof block === 'string') {
+        block = $(block);
+    }
+
+    // if exists at list one block
+    if (block.length) {
+        block.animate({
+            scrollTop: block.get(0).scrollHeight
+        }, duration);
+    }
+}
 function sendMessage(message){
     let jsonOb={
-        name:localStorage.getItem("name"),
+        name:name,
         content:message
     }
-    stompClient.send("/reciveroommessage",{},JSON.stringify(jsonOb));
+    if(message!="") {
+        stompClient.send("/reciveroommessage", {}, JSON.stringify(jsonOb));
+        document.getElementById("messagetxt").value="";
+        scrollToEnd('#message-content');
+    }
 }
 
 function connect()
@@ -26,7 +53,6 @@ function connect()
 
 function login(name)
 {
-    localStorage.setItem("name",name);
     connect();
 }
 function logout()
@@ -38,6 +64,11 @@ function logout()
 }
 function showMessage(message)
 {
+    let color="#f5f6f7";
+    if(message.name==name)
+    {
+        color="skyblue";
+    }
     console.log(message.name+": message is"+message.content);
     var content=`<div class="d-flex justify-content-between">
                             <p class="small mb-1">${message.name}</p>
@@ -47,10 +78,9 @@ function showMessage(message)
                             <img src="avtar.png"
                                  alt="avatar 1" style="width: 45px; height: 100%;">
                             <div>
-                                <p class="small p-2 ms-3 mb-3 rounded-3" style="background-color: #f5f6f7;">${message.content}</p>
+                                <p class="small p-2 ms-3 mb-3 rounded-3" style="background-color:${color};">${message.content}</p>
                             </div>
                         </div>`
     $('#message-content').append(content);
-
-
+    scrollToEnd('#message-content');
 }
