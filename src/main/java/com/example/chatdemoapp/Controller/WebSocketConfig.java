5cb2e8bebb68493e25f18287c81c9658a5d1d8d2
saket <saketbbu01@outlook.com>
2023-07-28 00:58:01 +0500

    new

--- src/main/java/com/example/chatdemoapp/Controller/WebSocketConfig.java
+++ src/main/java/com/example/chatdemoapp/Controller/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.example.chatdemoapp.Controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,14 +13,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/connect").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/connect").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
-
-
 
     }
 }
